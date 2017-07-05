df5ee3d830eba99098654c8352ed8f69b76a2797
俊 池邊 <shun.ikebe@avantcorp.com>
2017-07-05 10:12:48 +0900

    プロジェクト ファイルを追加します。

---
+++ MultiplicationTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\t* * * 九九の表 * * *");
+            Console.WriteLine("\t|\t1\t2\t3\t4\t5\t6\t7\t8\t9");
+            Console.WriteLine("--------------------------------------------------------------------------------------------------");
+
+            for (int a = 1; a < 10; a++)
+            {
+                for (int b = 1; b < 10; b++)
+                {
+                    if (b == 1) Console.Write(a + "\t|\t");
+                    Console.Write(a * b + "\t");
+                }
+                Console.Write("\n");
+            }
+
+        }
+    }
+}
